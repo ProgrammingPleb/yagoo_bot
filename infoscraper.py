83da9ac064456ef11626c4450433f677b3ea8d2d
Programming Pleb <ezzprograms+dev@gmail.com>
2021-01-14 17:39:03 +0800

    Added simple Fandom scraper

--- infoscraper.py
+++ infoscraper.py
@@ -1,5 +1,4 @@
-import json, aiohttp, asyncio, re
-import logging
+import json, aiohttp, asyncio, re, urllib.parse
 from bs4 import BeautifulSoup
 from typing import Union
 
@@ -87,9 +86,44 @@ async def channelInfo(channelId: Union[str, int]):
         }
     return channelData
 
+async def channelScrape(query: str):
+
+    if "/channel/" in query:
+        query = query.split("/")[-1]
+    
+    chInfo = await channelInfo(query)
+    if chInfo["success"] == False:
+        return {
+            "success": False
+        }
+    else:
+        chNSplit = chInfo["name"].split()
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f'https://virtualyoutuber.fandom.com/wiki/Special:Search?query={urllib.parse.quote_plus(chInfo["name"])}') as r:
+                soup = BeautifulSoup(await r.text(), "html5lib")
+                sArticles = soup.find("ul", {"class": "unified-search__results"}).find_all("article")
+                chLink = None
+                for article in sArticles:
+                    for name in chNSplit:
+                        if name in article.text:
+                            chLink = article.find("a")["href"]
+            async with session.get(chLink) as r:
+                with open("test/debug.html", "w", encoding="utf-8") as f:
+                    f.write(await r.text())
+                soup = BeautifulSoup(await r.text(), "html5lib")
+                infobox = soup.find("aside", {"class": "portable-infobox"})
+
+        return {
+            "success": True,
+            "gender": re.sub('\[\d+\]', '', infobox.find("div", {"data-source": "gender"}).find("div").text.replace('\n', '')),
+            "height": re.sub('\[\d+\]', '', infobox.find("div", {"data-source": "height"}).find("div").text.replace('\n', '')),
+            "age": re.sub('\[\d+\]', '', infobox.find("div", {"data-source": "age"}).find("div").text.replace('\n', '')),
+            "birthday": re.sub('\[\d+\]', '', infobox.find("div", {"data-source": "birthday"}).find("div").text.replace('\n', ''))
+        }
+
 def sInfoAdapter(id):
-    cData = asyncio.run(channelInfo(id))
+    cData = asyncio.run(channelScrape(id))
     print(cData)
 
 if __name__ == "__main__":
-    sInfoAdapter("UC1DCedRgGHBdm81E1llLhOQ")
+    sInfoAdapter("https://www.youtube.com/channel/UCt30jJgChL8qeT9VPadidSw")
