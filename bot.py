0945be6b77590af3f05992f3ce644ceb92a863ec
Programming Pleb <dev@pleb.moe>
2021-01-16 01:34:57 +0800

    Initial implementation on birthday notification

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import aiohttp, discord, asyncio, json, yaml, logging, sys, imgkit, os
+import aiohttp, discord, asyncio, json, yaml, logging, sys, imgkit, os, pytz
 from datetime import datetime
 from discord.ext.commands.core import command
 from itertools import islice
@@ -243,6 +243,44 @@ class msCycle(commands.Cog):
             logging.info("Notifying channels (Milestone).")
             await milestoneNotify(msData)
         logging.info("Milestone checks done.")
+
+async def bdayCheck():
+    update = False
+    write = False
+    bdayResults = {}
+
+    with open("data/bot.json") as f:
+        bdata = json.load(f)
+    
+    if "bdayCheck" not in bdata:
+        bdata["bdayCheck"] = datetime.now(pytz.timezone("Asia/Tokyo")).strftime("%m%d")
+        write = True
+
+    now = datetime.now(pytz.timezone("Asia/Tokyo")).replace(hour=0, minute=0, second=0, microsecond=0)
+    if pytz.timezone("Asia/Tokyo").localize(datetime.strptime(bdata["bdayCheck"], "%m%d").replace(year=datetime.now(pytz.timezone("Asia/Tokyo")).year)) < now:
+        with open("data/birthdays.json") as f:
+            bdays = json.load(f)
+
+        if str(now.day) in bdays[str(now.month)]:
+            bdayCurrent = bdays[str(now.month)][str(now.day)]
+            update = True
+        
+        bdata["bdayCheck"] = datetime.now(pytz.timezone("Asia/Tokyo")).strftime("%m%d")
+        write = True
+    
+    if write:
+        with open("data/bot.json") as f:
+            json.dump(bdata, f)
+    
+    if update:
+        return {
+            "occurToday": update,
+            "bdayData": bdayCurrent
+        }
+    else:
+        return {
+            "occurToday": update
+        }
 
 @bot.event
 async def on_ready():
